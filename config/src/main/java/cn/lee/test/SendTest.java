6b0b92c434cddb84537b5aa34ed5190dd5565ed4
Lee-eau <choaya@qq.com>
2022-08-27 18:53:58 +0800

    system.config

--- config/src/main/java/cn/lee/test/SendTest.java
+++ config/src/main/java/cn/lee/test/SendTest.java
@@ -1,14 +1,19 @@
 package cn.lee.test;
 
+import cn.hutool.http.HttpRequest;
+import cn.hutool.http.HttpResponse;
 import cn.hutool.http.HttpUtil;
 
 import java.util.HashMap;
 
 public class SendTest {
     public static void main(String[] args) throws InterruptedException {
+        HashMap<String,Object> map = new HashMap<>();
         while (true){
             String url = "http://localhost:1010/acturator/bus-refresh";
-            String post = HttpUtil.post(url,new HashMap<>());
+            HttpRequest request = HttpUtil.createPost(url);
+            request.contentType("application/json");
+            HttpResponse response = request.execute();
             Thread.sleep(1000 * 10);
         }
     }
